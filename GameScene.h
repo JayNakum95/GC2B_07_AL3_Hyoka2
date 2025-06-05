3d5431d08750d3f77fe7e38900a97bd1c9bc46ca
j n <jaynakumfb@gmail.com>
2025-06-06 02:24:43 +0900

    01_01

---
+++ GameScene.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "KamataEngine.h"
+
+class GameScene {
+
+	public:
+	GameScene();
+	~GameScene();
+	void Initialize();
+	void Update();
+	void Draw();
+};
--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <Windows.h>
 #include "KamataEngine.h"
+#include "GameScene.h"
 using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	KamataEngine::Initialize(L"GC2B_07_ナクム_ジェイ_ハルシュバルダン");
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize(); // ゲームシーンの初期化
 	while (true) {
 		if (KamataEngine::Update()) {
 
 			break;
 		} // エンジンの更新がfalseを返した場合、ループを終了		
-
+		gameScene->Update(); // ゲームシーンの更新
 		dxCommon->PreDraw();
-
+		gameScene->Draw(); // ゲームシーンの描画
 		dxCommon->PostDraw();
 	}
 	
+
+	delete gameScene; // ゲームシーンの解放
+	gameScene = nullptr; // ポインタをnullptrに設定
 
 	KamataEngine::Finalize();
 
