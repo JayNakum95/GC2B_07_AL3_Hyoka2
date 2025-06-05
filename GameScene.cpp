b47a0a7dbe05e31563d7870443e793cd28fb8c91
j n <jaynakumfb@gmail.com>
2025-06-06 03:08:21 +0900

    01_03

--- GameScene.cpp
+++ GameScene.cpp
@@ -12,12 +12,25 @@ GameScene::~GameScene() {
 
 	//delete sprite_; // スプライトオブジェクトの解放
 	//sprite_ = nullptr; // ポインタをnullptrに設定
+	delete model_; // モデルオブジェクトの解放
+	model_ = nullptr; // ポインタをnullptrに設定
+	delete debugCamera_; // デバッグカメラオブジェクトの解放
+	debugCamera_ = nullptr; // ポインタをnullptrに設定
 }
 void GameScene::Initialize() {
 	// 初期化処理の実装
 
 	textureHandle_ = TextureManager::Load("uvChecker.png"); // テクスチャの読み込み
 	//sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	model_ = Model::Create(); // モデルの読み込み
+	worldTransform_.Initialize(); // ワールド変換の初期化
+	camera_.Initialize();         // カメラの初期化
+	debugCamera_ = new DebugCamera(1280,720); // デバッグカメラの初期化
+	       // デバッグカメラの初期化
+	
+	soundDataHandle_ = Audio::GetInstance()->LoadWave("fanfare.wav"); // サウンドの読み込み
+	Audio::GetInstance()->PlayWave(soundDataHandle_);                 // サウンドの再生
+	voiceHandle_ = Audio::GetInstance()->PlayWave(soundDataHandle_,true);      // ボイスの読み込み
 }
 void GameScene::Update() {
 	// 更新処理の実装
@@ -27,10 +40,19 @@ void GameScene::Update() {
 	position.y += 1.0f; // y座標を0.5fだけ増加
 	sprite_->SetPosition(position); // スプライトの位置を更新
 	*/
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+	Audio::GetInstance()->StopWave(voiceHandle_); // ボイスの停止
+	}
+	debugCamera_->Update(); // デバッグカメラの更新
 }
 void GameScene::Draw() {
 	// 描画処理の実装
 	//Sprite::PreDraw();
 	//sprite_->Draw(); // スプライトの描画
 	//Sprite::PostDraw();
+
+	Model::PreDraw();
+	model_->Draw(worldTransform_, debugCamera_->GetCamera(), textureHandle_); // モデルの描画
+	Model::PostDraw();
+
 }
--- GameScene.h
+++ GameScene.h
@@ -13,8 +13,10 @@ class GameScene {
 	private:
 	uint32_t textureHandle_=0; // テクスチャハンドル
 	///KamataEngine::Sprite* sprite_ = nullptr; // スプライトオブジェクト
-
-	
-
-
+	KamataEngine::Model* model_ = nullptr; // モデルオブジェクト
+	KamataEngine::WorldTransform worldTransform_; // ワールド変換
+	KamataEngine::Camera camera_;  // カメラオブジェクト
+	KamataEngine::DebugCamera* debugCamera_=nullptr;       // デバッグカメラオブジェクト
+	uint32_t soundDataHandle_ = 0;                    // サウンドハンドル
+	uint32_t voiceHandle_ = 0;     // ボイスハンドル（オーディオ用）
 };
